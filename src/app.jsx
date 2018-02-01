afb06bd9ca34c34fdd334fa0a47cd09424d52f32
Marcus Grant <marcusfg@gmail.com>
2018-02-01 17:23:28 -0500

    Functioning ListCells
    * List cells now work
      * Default Cells render as expected
      * ListCells chose DefaultCells if no children
      * When using children...
        * ListCell's children takes callbacks properly
    * Cells are now in file src/lists/list-cell.jsx
    * Lists are now in file src/lists/lists.jsx
    * All list-based components now have own SASS file
      * located in src/lists/list.scss

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
-import { DefaultListCell, ListCell } from './lists/list-ui';
+import { DefaultListCell, ListCell } from './lists/list-cell';
+
+const defCell = <DefaultListCell label="Main Text" detailLabel="Subtext" />;
+const BasicCell = <h1>Basic Header Cell</h1>;
+const CallbackContent = props => (
+  <div>
+    <h1>This is a button callback</h1>
+    <button onClick={props.callback}>Toggle</button>
+  </div>
+);
 
 export default class extends React.Component {
   constructor(props) {
     super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      toggle: false,
+    };
+  }
+
+  toggle() {
+    this.setState({ toggle: !this.state.toggle });
   }
 
   render() {
     return (
       <div className="app__container">
-        <DefaultListCell label="Main Text" detailLabel="Subtext" />
+        <ListCell>
+          <CallbackContent callback={() => this.toggle()} />
+        </ListCell>
+        <h4>{this.state.toggle ? 'yes' : 'no'}</h4>
       </div>
     );
   }
