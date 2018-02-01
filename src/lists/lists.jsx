4ebcb55ce5c91f2550c001e4280a74b26a27037d
Marcus Grant <marcusfg@gmail.com>
2018-02-01 17:59:54 -0500

    First work on ListUI, NEEDS WORK
    - A good cell rendering methodology is needed
    - Consider how UITableViewController does it
    - Consider creating a function that when iterated maps them

--- src/lists/lists.jsx
+++ src/lists/lists.jsx
@@ -1,3 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { ListCell, DefaultListCell } from './list-cell';
+import './list.scss';
+
+const renderCells = cellRenderer => (
+  {}
+);
+
+const ListUI = (props) => {
+};
+
+// TODO: consider feeding cells with...
+//  - enumeration object
+//      - expected properties define nesting and structure
+//  - arrays of cells
+ListUI.propTypes = {
+  cells: PropTypes.instance
+};
+
+export default ListUI;
