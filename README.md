2d240ec85a9e5e19bf60412edde0e5ab83e47425
Marcus Grant <marcusfg@gmail.com>
2018-02-01 13:10:12 -0500

    1st commit

---
+++ README.md
@@ -0,0 +1,16 @@
+React Collection Components
+===========================
+A set of modules containing React components that are focused on creating a uniform structure when rendering UI's that are structured as one dimensional Lists or Bars, or in two dimensions Grids or Flows. And to go with this backing structure various specific compositions of these things and other code to make them better suited for specific situations like dropdown menus, sidebars, header/footer bars, galleries, carousells, cards, etc. I came from a background of iOS software development before going into Full Stack development and enjoyed the structure of Apple's Cocoa Frameworks' [`UICollectionView`][01] and its subclasses like `UITableViewController`.
+
+Lists
+-----
+
+
+
+References
+----------
+[01]: https://developer.apple.com/documentation/uikit/uicollectionview "Apple Docs: UICollectionView"
+[02]: https://developer.apple.com/documentation/uikit/uitableview "Apple Docs: UITableView"
+
+1. [Apple Docs: UICollectionView][01]
+2. [Apple Docs: UITableView][02]
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const paths = {
+  ROOT: path.resolve(__dirname),
+  DIST: path.resolve(__dirname, 'dist'),
+  SRC: path.resolve(__dirname, 'src'),
+  // SASS: path.resolve(__dirname, 'src/sass'),
+};
+
+const files = {
+  INDEX_JS: path.join(paths.SRC, 'index.jsx'),
+  INDEX_HTML: 'index.html',
+  MAIN_SCSS: path.join(paths.SRC, 'main.scss'),
+  BUNDLE: 'bundle.js',
+  DIST_CSS: 'styles.css',
+};
+
+// Webpack configs
+module.exports = {
+  entry: [files.INDEX_JS, files.MAIN_SCSS],
+  output: {
+    path: paths.DIST,
+    // TODO: make this app.bundle.js to be more clear
+    filename: files.BUNDLE,
+  },
+  devtool: 'eval-source-map',
+  // Loaders configs
+  // Babel Loaders
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        // Important to note, from http://bit.ly/2zoKnrv
+        // There needs to be a loader option somewhere
+        // This is inspite of the articles
+        // Then also, 'babel-loader' instead of 'babel' is needed
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      },
+      // TODO: Figure out how to properly extract regular css as loader
+      // { // regular css files
+      //   test: /\.css$/,
+      //   loader: ExtractTextPlugin.extract({
+      //     loader: 'css-loader?importLoaders=1',
+      //   }),
+      // },
+      { // sass / scss loader for webpack
+        test: /\.(sass|scss)$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader']),
+      },
+    ],
+  },
+  // Tell Webpack to use html & CSS extract text plugin
+  plugins: [
+    new ExtractTextPlugin({ // define where to save the file
+      filename: files.DIST_CSS,
+      allChunks: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: files.INDEX_HTML,
+    }),
+  ],
+  // Enable importing JS files without specifying their extensions
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
