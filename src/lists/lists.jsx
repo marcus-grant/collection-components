d1eb612eb6ea07ac3b6b9a9d337f6763a393ecf2
Marcus Grant <marcusfg@gmail.com>
2018-02-02 17:43:10 -0500

    Minor restructuring, new plan on collections
    - Swift & UIKit aren't as declarative...
    - Aren't as unidirectional in dataflows as React
    - Therefore React shouldn't be copying its...
      - Delegation of functions to render something
      - Its data passing techniques
    - Restructuring Lists so that they are just deeper iterators

--- src/lists/lists.jsx
+++ src/lists/lists.jsx
@@ -1,21 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ListCell, DefaultListCell } from './list-cell';
+
 import './list.scss';
 
-const renderCells = cellRenderer => (
-  {}
+const cellStyle = 'list-cell__container';
+
+const ListCellLabel = text =>
+  <p className="list-cell__label">{text}</p>;
+const ListCellDetailLabel = text =>
+  <p className="list-cell__label--detail">{text}</p>;
+
+// TODO: Consider an accessory ie a chevron or another composition of components
+export const DefaultListCell = props => (
+  <div className={cellStyle}>
+    <div className="list-cell__content">
+      {props.label && ListCellLabel(props.label)}
+      {props.detailLabel && ListCellDetailLabel(props.detailLabel)}
+    </div>
+  </div>
 );
 
-const ListUI = (props) => {
+DefaultListCell.defaultProps = {
+  label: undefined,
+  detailLabel: undefined,
 };
 
-// TODO: consider feeding cells with...
-//  - enumeration object
-//      - expected properties define nesting and structure
-//  - arrays of cells
-ListUI.propTypes = {
-  cells: PropTypes.instance
+DefaultListCell.propTypes = {
+  label: PropTypes.string,
+  detailLabel: PropTypes.string,
 };
 
-export default ListUI;
+export const ListCell = (props) => {
+  if (props.children) return <div className={cellStyle}>{props.children}</div>;
+  const { label, detailLabel } = props;
+  const defaultProps = { label, detailLabel };
+  return <DefaultListCell {...defaultProps} />;
+};
+
+ListCell.defaultProps = {
+  children: undefined,
+  label: undefined,
+  detailLabel: undefined,
+};
+
+ListCell.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node).isRequired,
+    PropTypes.node.isRequired,
+  ]),
+  label: PropTypes.string,
+  detailLabel: PropTypes.string,
+};
+
+export const ListSection = (props) => {
+
+};
+
+ListSection.propTypes = {
+  rows: PropTypes.arrayOf(),
+};
